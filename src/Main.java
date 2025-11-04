4b00ae9612e31ecfa999e51f8ce626879ab712ef
Brian_KIp <yobra8504@gmail.com>
2025-11-04 23:01:48 +0300

    Managing User input Using Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class Main {
+    public static void main() {
 
-    public static void main (String[] args){
-        System.out.print("Nigaaaa");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Your name ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi there " + name + " How are you doing ?");
+
+        System.out.print("Whats Your GPA "+ name +" ? ");
+        double gpa = scanner.nextDouble();
+        System.out.println("Congrats you got a " + gpa+" in your GPA");
     }
-
 }
