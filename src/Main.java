b3171c27092470f07251534509e1edb3ff764595
Brian_KIp <yobra8504@gmail.com>
2025-11-05 16:29:28 +0300

    Broken passage

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,39 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main() {
-//        Calculate Area of a Rectangle
-        double width = 0;
-        double height = 0;
-        double area = 0;
+    static void main() {
+//  MAD LIBS GAME
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("You wanna find the area of a Rectangle? (true/false): ");
-        boolean findArea = scanner.nextBoolean();
-        System.out.println();
+        String adjective1;
+        String adjective2;
+        String adjective3;
+        String verb1;
+        String noun1;
 
-        System.out.println("Wagwan G");
-        if (findArea){
-            System.out.println("Then fill out the following :) ");
-        }
-        else {
-            System.out.println("Feel free to leave");
-        }
+        System.out.print("Enter an adjective (Description): ");
+        adjective1 = scanner.nextLine();
 
-        System.out.print("Whats the width ");
-        width = scanner.nextDouble();
+        System.out.print("Enter a noun (Animal/ Person): ");
+        noun1 = scanner.nextLine();
 
-        System.out.print("Whats the Height ");
-        height = scanner.nextDouble();
+        System.out.print("Enter an adjective (Description): ");
+        adjective2 = scanner.nextLine();
 
-        area = width * height;
-        System.out.println("Here is your area: "+area+" ");
+        System.out.print("Enter a verb ending with -ing (action): ");
+        verb1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (Description): ");
+        adjective3 = scanner.nextLine();
+
+
+        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + " !");
+        System.out.println("I was " + adjective3 + "!");
+
+
         scanner.close();
     }
 }
